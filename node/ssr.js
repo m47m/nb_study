0e31df2bcdc0c7dd31750101fd5992f9dc1f8332
m47m <xm130812@163.com>
2024-03-11 10:07:32 +0800

    [nb] Commit

---
+++ node/ssr.js
@@ -0,0 +1,27 @@
+const {JSDOM} = require('jsdom');
+const fs = require('node:fs');
+
+const root = new JSDOM(`<!DOCTYPE html><html><head></head><body>
+	<div id='app'></div>
+</body></html>`);
+
+const window = root.window;
+const document = window.document;
+const app = document.querySelector('#app');
+// 请求接口
+fetch('https://api.thecatapi.com/v1/images/search?limit=10&page=1').then(res=>res.json()).then(data=>{
+	data.forEach(element => {
+		const img = document.createElement('img');
+		img.src = element.url;
+		img.style.width = '200px';
+		img.style.height = '200px';
+
+		app.appendChild(img);
+	});
+	// 将序列化后的内容 写入文件
+	//生成html文件
+	fs.writeFileSync('./ssr_index.html',root.serialize());
+
+	console.log(root.serialize());
+});
+
