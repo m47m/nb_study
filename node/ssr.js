fa54b5dd418e3d13e22d9c1731ceb463ad4f0736
m47m <xm130812@163.com>
2024-03-24 11:21:11 +0800

    [nb] Commit

--- node/ssr.js
+++ node/ssr.js
@@ -1,5 +1,5 @@
-const {JSDOM} = require('jsdom');
-const fs = require('node:fs');
+const { JSDOM } = require("jsdom");
+const fs = require("node:fs");
 
 const root = new JSDOM(`<!DOCTYPE html><html><head></head><body>
 	<div id='app'></div>
@@ -7,21 +7,22 @@ const root = new JSDOM(`<!DOCTYPE html><html><head></head><body>
 
 const window = root.window;
 const document = window.document;
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 // 请求接口
-fetch('https://api.thecatapi.com/v1/images/search?limit=10&page=1').then(res=>res.json()).then(data=>{
-	data.forEach(element => {
-		const img = document.createElement('img');
-		img.src = element.url;
-		img.style.width = '200px';
-		img.style.height = '200px';
+fetch("https://api.thecatapi.com/v1/images/search?limit=10&page=1")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((element) => {
+      const img = document.createElement("img");
+      img.src = element.url;
+      img.style.width = "200px";
+      img.style.height = "200px";
 
-		app.appendChild(img);
-	});
-	// 将序列化后的内容 写入文件
-	// 生成html文件
-	fs.writeFileSync('./ssr_index.html',root.serialize());
+      app.appendChild(img);
+    });
+    // 将序列化后的内容 写入文件
+    // 生成html文件
+    fs.writeFileSync("./ssr_index.html", root.serialize());
 
-	console.log(root.serialize());
-});
-
+    console.log(root.serialize());
+  });
