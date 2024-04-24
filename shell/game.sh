5ca3c3f5768e0a0a21bbd0e9cbfc3c3e6ca2a11b
m47m <xm130812@163.com>
2024-04-24 11:16:19 +0800

    [nb] Commit

---
+++ shell/game.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "your name? "
+read name
+echo "hello,$name"
+
+echo "zero is $0"
---
+++ shell/is_prime.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/zsh
+
+echo "start"
+# 函数 检查一个数字是否为素数
+is_prime() {
+	local num=$1 
+
+	if [ $num -lt 2 ]; then
+		return 1
+
+	for ((i=2; i*i<=num; i++)); do
+		if [ $((num % i)) -eq 0 ]; then
+			return 1
+		fi
+	done 
+	return 0
+}
+
+read 'number?one number : '
+
+if is_prime $number; then
+	echo "the number $number is prime"
+else 
+	echo "the number $number is not prime"
+fi 
